676b245d07cd5a0f180cadd4ec3c3b39625155ad
ilian <iliankyuchukov@gmail.com>
2020-11-04 17:47:25 +0200

    removed unneeded import

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Exceptions\UserAlreadyExistsException;
 use App\Exceptions\UserNotFoundException;
 use App\Services\Authenticator;
